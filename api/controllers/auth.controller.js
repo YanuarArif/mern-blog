9a6c000ec99559d8609deb515c430f3c19a72552
Yanuar Lukman Arif <yanuarlukmanarif@gmail.com>
2024-01-21 22:31:50 +0700

    membuat signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ message: "Semua fields harus diisi" });
+  }
+
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json("SignUp Sukses!");
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
